937d94994d1d8662585341471ebae5b53e49a13c
Ollo <joshua@ollomedia.com>
2014-07-24 11:30:25 -0600

    add echos to show gh-page publish steps

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,8 +6,13 @@
 
 cd "$(dirname $0)";
 parent_sha=$(git show-ref -s refs/heads/gh-pages)
+echo $parent_sha
 doc_sha=$(git ls-tree -d HEAD styleguide | awk '{print $3}')
+echo $doc_sha
 new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha)
+echo $new_commit
+echo git update-ref refs/heads/gh-pages $new_commit
 git update-ref refs/heads/gh-pages $new_commit
-git push origin master
+echo git push origin gh-pages
+git push origin gh-pages
 exit;
